d7d8d75f9552224fdca37bbdad6407f40399fc72
malekak44 <malekak44@gmail.com>
2021-09-06 23:30:17 +0600

    just starting

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
+import React from 'react'
+import Header from './components/Header'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello {this.props.name}</h1>
-        <Header />
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
-
-export default App;
